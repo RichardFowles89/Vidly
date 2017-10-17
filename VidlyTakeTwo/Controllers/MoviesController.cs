fc6cd2279004c6609372d1ca23d3e8833e3e0a1d
RichardFowles89 <richardfowles89@gmail.com>
2017-10-17 20:39:40 +0100

    Mid way through exercise. About to implement lecture 80 to movies

--- VidlyTakeTwo/Controllers/MoviesController.cs
+++ VidlyTakeTwo/Controllers/MoviesController.cs
@@ -118,9 +118,10 @@ namespace VidlyTakeTwo.Controllers
 
         public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
+            //var movies = _context.Movies.Include(m => m.Genre).ToList();
 
-            return View(movies);
+            //return View(movies);
+            return View();
         }
 
         public ActionResult Details(int id)
